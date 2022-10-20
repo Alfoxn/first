62f615f41cdf66da632a6f8f9467de1ce778450c
Danila Kozlov <danila.kozlov23@yandex.ru>
2022-10-20 18:25:13 +0300

    Комментарии по калькулятору

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Human person1 = new Human("Alise", "Skv", "S", 26);
-        Human person2 = new Human();
-        System.out.println(person1.name);
-        person1.setName("Alex");
-        System.out.println(person1.getName());
-        person1.setSurname("S");
-        System.out.println(person1);
-        System.out.println(person2);
-
+//        Human person1 = new Human("Alise", "Skv", "S", 26);
+//        Human person2 = new Human();
+//        System.out.println(person1.name);
+//        person1.setName("Alex");
+//        System.out.println(person1.getName());
+//        person1.setSurname("S");
+//        System.out.println(person1);
+//        System.out.println(person2);
+//
         Calculator one = new Calculator();
-        one.Num1();
-        one.Num2();
-        one.SUM();
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        one.sum(num1, num2);
 
 
     }
